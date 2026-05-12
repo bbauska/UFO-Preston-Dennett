624275f6eeac4d5c7368929702caa6f7c2c609fe
Brian Bauska <brianbauska@gmail.com>
2026-05-12 16:00:33 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+/* js/scripts.js of UFO-Preston-Dennett for UFO-Preston-Dennett.bauska.org */
 var drawRipple = function(ev) {
   var node = document.querySelector(".ripple");
-	var newNode = node.cloneNode(true);
-	newNode.classList.add("animate");
+  var newNode = node.cloneNode(true);
+  newNode.classList.add("animate");
   newNode.style.Left = ev.clientX - 5 + "px";
-	newNode.style.top = ev.clientY - 5 + "px";
-	node.parentNode.replaceChile(newNode, node);
+  newNode.style.top = ev.clientY - 5 + "px";
+  node.parentNode.replaceChile(newNode, node);
 };
 window.addEventListener("click", drawRipple);
